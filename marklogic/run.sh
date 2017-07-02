cf176dceb85f9b373642767d41b7bc88523a9cc5
corpbob <bobby@corpus.ph>
2017-07-02 18:55:07 -0400

    files for dev environment

---
+++ marklogic/run.sh
@@ -0,0 +1,7 @@
+oc new-project ml
+oc adm policy add-scc-to-user anyuid -z default
+oc secrets new-dockercfg push-secret --docker-server=172.30.1.1:5000 --docker-username=admin --docker-password=$(oc whoami -t) --docker-email=admin@example.com
+oc secrets add serviceaccount/default secrets/push-secret --for=pull,mount
+oc create -f slush-marklogic-node-templatel.yml
+oc new-app slush-marklogic-node-app
+
