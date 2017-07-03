75b03624eddb04859ca7e646b94ed2782b1e78b6
corpbob <bobby@corpus.ph>
2017-07-03 04:36:42 -0400

    new files

--- marklogic/run.sh
+++ marklogic/run.sh
@@ -1,7 +1,9 @@
-oc new-project ml
+PROJECT=$1
+ENV=$2
+oc new-project ${PROJECT:ml-dev}
 oc adm policy add-scc-to-user anyuid -z default
 oc secrets new-dockercfg push-secret --docker-server=172.30.1.1:5000 --docker-username=admin --docker-password=$(oc whoami -t) --docker-email=admin@example.com
 oc secrets add serviceaccount/default secrets/push-secret --for=pull,mount
-oc create -f slush-marklogic-node-templatel.yml
+oc create -f slush-marklogic-node-templatel-${ENV:=dev}.yml
 oc new-app slush-marklogic-node-app
 
